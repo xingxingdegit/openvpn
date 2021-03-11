a054cb89ab7958b9e9509db4280efb9ee15d1415
xingxingdegit <xingxingdemail@foxmail.com>
2021-03-11 11:16:20 +0800

    设置crl的过期时间

--- openssl.sh
+++ openssl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #conf 目录存储了openssl配置文件，是脚本使用的。还有openvpn.conf client.conf 是openvpn的服务端与客户端的配置文件例子。
-#openvpn conf/openvpn.conf        # 启动openvpn。这里只是一个例子，大家可以相应修改里面的证书与key还有dh文件和log所对应的文件与目录。
+#openvpn conf/openvpn.conf        # 启动openvpn。这里只是一个例子，大家可以相应修改里面的证书与key还有dh文件和log所对应的文件与目录。
 #相关证书存储在CA/certs里面。
 #./openssl.sh init <name>         # ./openssl.sh init ca.atest.pub
 #./openssl.sh dh                  # ./openssl.sh dh
-#./openssl.sh build <name>        # ./openssl.sh build xiaomei    openvpn客户端所用证书。
-#./openssl.sh build <server>      # 跟上面一行一样， 只是生成一个证书，也可以用别的名子，openvpn用来做为server证书。
-#./openssl.sh revoke <name>       # 注销证书
+#./openssl.sh build <name>        # ./openssl.sh build xiaomei    openvpn客户端所用证书。
+#./openssl.sh build <server>      # 跟上面一行一样， 只是生成一个证书，也可以用别的名子，openvpn用来做为server证书。
+#./openssl.sh revoke <name>       # 注销证书
 #./openssl.sh revoke <name> -f    # 注销证书，上面注销失败的话。
 
 
@@ -18,10 +18,15 @@ export CA_DIR="$(pwd)/CA"                   # CA目录
 export KEY_COUNTRY="CN"                     # 国家
 export KEY_PROVINCE="BJ"                    # 省市
 export KEY_CITY="BJ"                        # 城市
-export KEY_ORG="atest.pub"                      # 组织
+export KEY_ORG="qfpay.com"                      # 组织
 export KEY_OUNAME="op"                      # 部门名称
-export KEY_EMAIL="op@atest.pub"             # 邮件
-export KEY_COMMONNAME="ca.atest.pub"            # 主机名,没用，在下面还会指定
+export KEY_EMAIL="op@qfpay.com"             # 邮件
+export KEY_COMMONNAME="vpnserver"            # 主机名,没用，在下面还会指定
+
+# CA证书有效期，crl证书有效期，用户证书有效期
+CA_expire=3650
+CRL_expire=3650
+User_expire=3650
 
 if [ $# -eq 0 ];then
     echo "Usage: $0 {init <name> | build <name> [san]} | revoke <name> | reinit | dh}"
@@ -51,8 +56,8 @@ env_pre () {
 ca_init () {
     env_pre && \
     (umask 077;openssl genrsa -out $CA_DIR/private/cakey.pem 4096) && \
-    openssl req -config $openssl_conf -new -x509 -sha256 -key $CA_DIR/private/cakey.pem -out $CA_DIR/cacert.pem -days 3650 && \
-    openssl ca -config $openssl_conf -gencrl -out $CA_DIR/crl.pem && \
+    openssl req -config $openssl_conf -new -x509 -sha256 -key $CA_DIR/private/cakey.pem -out $CA_DIR/cacert.pem -days $CA_expire && \
+    openssl ca -config $openssl_conf -gencrl -out $CA_DIR/crl.pem -crldays $CRL_expire&& \
     echo -e "\033[32mcreate ca cert is successful\033[0m" || echo -e "\033[31mcreate ca cert is failure\033[0m"
 }
 
@@ -66,9 +71,9 @@ build_user () {
     (umask 077;openssl genrsa -out $store_path/$1.key 2048) && \
     openssl req -config $openssl_conf -new -key $store_path/$1.key -out $store_path/$1.csr && \
     if [ "$2" == "san" ];then
-        openssl ca -config $openssl_conf -extensions v3_req -md sha256 -in $store_path/$1.csr -out $store_path/$1.crt -days 3650
+        openssl ca -config $openssl_conf -extensions v3_req -md sha256 -in $store_path/$1.csr -out $store_path/$1.crt -days $User_expire
     else
-        openssl ca -config $openssl_conf -md sha256 -in $store_path/$1.csr -out $store_path/$1.crt -days 3650
+        openssl ca -config $openssl_conf -md sha256 -in $store_path/$1.csr -out $store_path/$1.crt -days $User_expire
     fi && \
     rm -f $store_path/$1.csr && \
     echo -e "\033[32mcreate $1 cert is successful\033[0m" || echo -e "\033[31mcreate $1 cert is failure\033[0m" && \
@@ -80,8 +85,10 @@ build_user () {
     cp $store_path/$1.crt $userdir/
     cp $store_path/$1.key $userdir/
     cp $CA_DIR/cacert.pem $userdir/
-    cd usertmp && tar -cf $1.tar $1 && rm -rf $1 && echo "wget http://172.100.102.77/k8s/$1.tar"
-    
+    cp conf/client.conf $userdir/
+    sed -i "s/xiaoli/$1/g" $userdir/client.conf
+
+
 }
 
 
@@ -117,7 +124,7 @@ revoke_user () {
 }
 
 build_dh () {
-    openssl dhparam -outform pem -out $CA_DIR/dh1024.pem 1024 && \
+    openssl dhparam -outform pem -out $CA_DIR/dh2048.pem 2048 && \
     echo -e "\033[32mcreate dh pem is successful\033[0m" || \
     echo -e "\033[31mcreate dh pem is failure\033[0m"
 }
@@ -136,7 +143,7 @@ case $1 in
     build)
         if [ -n "$2" ];then
             export KEY_COMMONNAME=$2
-            build_user $2 $3
+            build_user $2
         else
             echo "Usage: $0 {init <name> | build <name> } | revoke <name> | reinit | dh}"
         fi
@@ -154,3 +161,4 @@ case $1 in
         build_dh  ;;
 
 esac
+
